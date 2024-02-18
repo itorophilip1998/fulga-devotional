6914089c4f3bf38d6b4457acdedb50f696c8b7d2
Itorophilip1998 <itorophilip1998@gmail.com>
2024-02-18 18:35:41 +0100

    dev

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
+import { StyleSheet, StatusBar, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
+import { AppLoading } from 'expo';
 
 export default function App() {
   const [url] = useState('http://devotional-beta.netlify.app');
   const [webKey] = useState(1);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    // Simulate an asynchronous task (e.g., fetching data, initializing resources)
+    const simulateAsyncTask = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          // Simulated task completed
+          resolve();
+        }, 3000); // Adjust the duration in milliseconds
+      });
+    };
+
+    // Start the asynchronous task
+    const startAsyncTask = async () => {
+      await simulateAsyncTask();
+      setAppIsReady(true);
+    };
+
+    startAsyncTask();
+  }, []);
+
+  if (!appIsReady) {
+    // The app is not yet ready, show a loading indicator or splash screen
+    return <AppLoading />;
+  }
 
 
 
 
   return (
-    <>
+    <React.Fragment>
       {/* <SafeAreaView style={styles.safeArea}> */}
       {/* Your content inside the SafeAreaView */}
       <StatusBar backgroundColor="#3f51b5" barStyle="light-content" />
@@ -22,7 +49,7 @@ export default function App() {
         source={{ uri: url }}
       />
       {/* </SafeAreaView> */}
-    </>
+    </React.Fragment>
 
 
 
